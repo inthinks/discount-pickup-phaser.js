87f5569e4d8c380097b3ac298b9af5c53c88da23
inthink <ind_ari08@yahoo.com>
2023-10-20 06:57:02 +0700

    add assets

---
+++ js/splashScene.js
@@ -0,0 +1,38 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Splash Scene
+
+class SplashScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "splashScene" });
+
+    this.splashSceneBackgroundImage = null;
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff");
+  }
+
+  preload() {
+    console.log("Splash Scene");
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png");
+  }
+
+  create(data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, "splashSceneBackground");
+    this.splashSceneBackgroundImage.x = 1920 / 2;
+    this.splashSceneBackgroundImage.y = 1080 / 2;
+  }
+
+  update(time, delta) {
+    if (time > 3000) {
+      this.scene.switch("menuScene");
+    }
+  }
+}
+
+export default SplashScene;
