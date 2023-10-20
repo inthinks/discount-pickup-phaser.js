8ad12f2c4d776dd1495e5125a9dfdeb2796418e8
yui <712020:dea5f8a9-4e63-45d7-84f2-d03ca920b6d8@bots.bitbucket.org>
2023-10-20 10:38:20 +0700

    add info

--- js/menuScene.js
+++ js/menuScene.js
@@ -7,40 +7,63 @@
 // This is the Menu Scene
 
 class MenuScene extends Phaser.Scene {
-  constructor () {
-    super({ key: 'menuScene' })
+  constructor() {
+    super({ key: "menuScene" });
 
-    this.menuSceneBackgroundImage = null
-    this.startButton = null
+    this.menuSceneBackgroundImage = null;
+    this.startButton = null;
   }
 
-  init (data) {
-    this.cameras.main.setBackgroundColor('#ffffff')
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff");
   }
 
-  preload () {
-    console.log('Menu Scene')
+  preload() {
+    console.log("Menu Scene");
 
-    this.load.image('menuSceneBackground', 'assets/menu-scene.jpg')
-    this.load.image('startButton', 'assets/start.png')
+    this.load.image("menuSceneBackground", "assets/menu-scene.jpg");
+    this.load.image("startButton", "assets/start.png");
+
+    this.load.atlas("ui", "assets/nine-slice.png", "assets/nine-slice.json");
   }
 
-  create (data) {
-    this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground')
-    this.menuSceneBackgroundImage.x = 1920 / 2
-    this.menuSceneBackgroundImage.y = 1080 / 2
+  create(data) {
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground");
+    this.menuSceneBackgroundImage.x = 1920 / 2;
+    this.menuSceneBackgroundImage.y = 1080 / 2;
 
-    this.startButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
-    this.startButton.setInteractive({ useHandCursor: true })
-    this.startButton.on('pointerdown', () => this.clickButton())
+    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton");
+    this.startButton.setInteractive({ useHandCursor: true });
+    this.startButton.on("pointerdown", () => this.clickButton());
+
+    const panel = this.add.nineslice(1920 / 2, 300, "ui", "PopupBackground400", 500, 400, 160, 160, 100, 100);
+
+    const content = ["Dapatkan Discount up to 30%", "Dengan memilih dua dari box uniqlo", "Anda hanya akan diberi kesempatan dua kali untuk memilih box yang berisikan nominal discount", "Selamat mencoba!", "Semoga Beruntung!"];
+
+    const text = this.add.text(1920 / 2, 300, content, { align: "center" });
+
+    text.setOrigin(0.5, 0.5);
+    text.setWordWrapWidth(300);
+
+    this.tweens.add({
+      targets: panel,
+      width: 800,
+      height: 500,
+      duration: 3000,
+      ease: "sine.inout",
+      yoyo: false,
+      repeat: 0,
+      onUpdate: () => {
+        text.setWordWrapWidth(panel.width - 100);
+      },
+    });
   }
 
-  update (time, delta) {
-  }
+  update(time, delta) {}
 
-  clickButton () {
-    this.scene.start('gameScene')
+  clickButton() {
+    this.scene.start("gameScene");
   }
 }
 
-export default MenuScene
+export default MenuScene;
